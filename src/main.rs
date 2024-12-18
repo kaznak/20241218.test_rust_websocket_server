c0f124ec1f0b9ef9254f4bd86b14d87ba21ff21d
kaznak <kaznak.at.work@ipebble.org>
2024-12-18 14:29:37 +0900

    refine

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,27 @@ use tungstenite::accept;
 
 /// A WebSocket echo server
 fn main() {
-    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    let server = TcpListener::bind("127.0.0.1:9001").expect("Failed to bind");
+    println!("WebSocket server listening on ws://127.0.0.1:9001");
     for stream in server.incoming() {
         spawn(move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
+            let mut websocket = accept(stream.expect("Failed to accept connection"))
+                .expect("Failed to accept websocket connection");
             loop {
-                let msg = websocket.read().unwrap();
-
+                let msg = match websocket.read() {
+                    Ok(m) => m,
+                    Err(e) => {
+                        eprintln!("Error reading message: {}", e);
+                        break;
+                    }
+                };
+                println!("Received: {}", msg);
                 // We do not want to send back ping/pong messages.
                 if msg.is_binary() || msg.is_text() {
-                    websocket.send(msg).unwrap();
+                    if let Err(e) = websocket.send(msg) {
+                        eprintln!("Error writing message: {}", e);
+                        break;
+                    }
                 }
             }
         });
